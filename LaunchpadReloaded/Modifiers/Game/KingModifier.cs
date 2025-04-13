4ef010986614a9b201deb04defb321bc9016b05f
AndriesWorks <andries46565@gmail.com>
2025-04-13 18:20:43 +0200

    added king modifier (VIP)

---
+++ LaunchpadReloaded/Modifiers/Game/KingModifier.cs
@@ -0,0 +1,22 @@
+﻿using LaunchpadReloaded.Components;
+using LaunchpadReloaded.Options.Modifiers;
+using MiraAPI.GameOptions;
+using UnityEngine;
+
+namespace LaunchpadReloaded.Modifiers.Fun;
+
+public sealed class KingModifier : LPModifier
+{
+    public override string ModifierName => "VIP";
+    public override int GetAssignmentChance() => (int)OptionGroupSingleton<GameModifierOptions>.Instance.KingChance;
+    public override int GetAmountPerGame() => 1;
+
+    public override void OnActivate()
+    {
+        PlayerControl.LocalPlayer.RpcSetHat("hat_NewYear2024");
+    }
+    public override void OnDeactivate()
+    {
+        PlayerControl.LocalPlayer.RpcSetHat("");
+    }
+}
