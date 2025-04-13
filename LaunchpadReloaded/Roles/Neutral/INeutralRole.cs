535cf79a255ddd7af1a5815210b6b1acfd8d04b7
AndriesWorks <andries46565@gmail.com>
2025-04-13 18:20:27 +0200

    updated from IOutcastRole to INeutralRole

---
+++ LaunchpadReloaded/Roles/Neutral/INeutralRole.cs
@@ -0,0 +1,11 @@
+﻿using MiraAPI.Roles;
+using UnityEngine;
+
+namespace LaunchpadReloaded.Roles.Neutral;
+
+public interface INeutralRole : ICustomRole
+{
+    ModdedRoleTeams ICustomRole.Team => ModdedRoleTeams.Custom;
+    RoleOptionsGroup ICustomRole.RoleOptionsGroup => new("Neutral Roles", Color.gray);
+    TeamIntroConfiguration? ICustomRole.IntroConfiguration => new(Color.gray, "NEUTRAL", "You are a Neutral. You do not have a team.");
+}
